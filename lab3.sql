288853c2de4cec316d7ca67ac98e3a76e803810c
Matías <m_grob@hotmail.com>
2022-12-05 19:15:25 +0100

    lab3

--- lab3.sql
+++ lab3.sql
@@ -2,7 +2,24 @@
 
 USE sakila;
 
+-- 1)Drop column picture from staff.
+ALTER TABLE staff
+DROP COLUMN picture;
 
+-- 2)A new person is hired to help Jon. Her name is TAMMY SANDERS, 
+-- and she is a customer. Update the database accordingly.
+
+INSERT INTO staff(staff_id,first_name,last_name,address_id,email,store_id,active,username,password,last_update) 
+VALUES (3,"TAMMY","SANDERS",79,"tammy.sanders@sakilacustomer.org",2,1,"TAMMY",0,"2006-02-15 04:57:20");
+
+SELECT *
+FROM STAFF;
+
+-- 3)Add rental for movie "Academy Dinosaur" by Charlotte Hunter from Mike Hillyer at Store 1. You can use current date for the rental_date column in the rental table. Hint: Check the columns in the table rental and see what information you would need to add there. You can query those pieces of information. For eg., you would notice that you need customer_id information as well. To get that you can use the following query:
+-- select customer_id from sakila.customer
+-- where first_name = 'CHARLOTTE' and last_name = 'HUNTER';
+
+-- Activity 2
 
 
 
